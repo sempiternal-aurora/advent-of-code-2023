6bb4ace932fd1b0602ce316a3a19bf74bfcbacee
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2024-04-10 19:42:28 +1000

    Complete part 1 day 7

--- day7/day7.c
+++ day7/day7.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef enum Card {TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE} Card;
 
@@ -25,14 +28,49 @@ Card parseCard(char card) {
 }
 
 HandType getType(HandBid * handBid) {
-    int numCards[13];
-    for (int i = 0; i < 13; i++) {
-        numCards[i] = 0;
-    }
+    int numCardsBuffer[13] = {0,0,0,0,0,0,0,0,0,0,0,0,0};
     for (int i = 0; i < 5; i++) {
-        numCards[handBid->ordering[i]] = numCards[handBid->ordering[i]] + 1;
+        numCardsBuffer[handBid->ordering[i]] = numCardsBuffer[handBid->ordering[i]] + 1;
+    }
+    bool has_two = false;
+    bool has_three = false;
+    for (int i = 0; i < 13; i++) {
+        switch (numCardsBuffer[i]) {
+            case 5: return QUINTET;
+            case 4: return QUARTET;
+            case 3:
+                if (has_two) {
+                    return FULL_HOUSE;
+                } else {
+                    has_three = true;
+                }
+                break;
+            case 2:
+                if (has_three) {
+                    return FULL_HOUSE;
+                }
+                else if (has_two) {
+                    return TWO_PAIR;
+                }
+                else {
+                    has_two = true;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    if (has_three) {
+        return TRIPLE;
+    }
+    else if (has_two) {
+        return ONE_PAIR;
+    }
+    else {
+        return HIGH_CARD;
     }
 }
+
 int compareRank(HandBid * handBid1, HandBid * handBid2) {
     HandType type1 = getType(handBid1);
     HandType type2 = getType(handBid2);
@@ -46,6 +84,7 @@ int compareRank(HandBid * handBid1, HandBid * handBid2) {
                 return handBid1->ordering[comparing] - handBid2->ordering[comparing];
             }
         }
+        return 0;
     }
     else {
         return type1 - type2;
@@ -57,8 +96,15 @@ LinkedHandBidList * insertSorted(LinkedHandBidList * start, LinkedHandBidList * 
     LinkedHandBidList * nextPtr = start;
     LinkedHandBidList * pastPtr = NULL;
     while (nextPtr) {
-        if (compareRank(&start->handBid, &newItem->handBid) > 0) {
-
+        if (compareRank(&nextPtr->handBid, &newItem->handBid) >= 0) {
+            newItem->next = nextPtr;
+            if (pastPtr == NULL) {
+                return newItem;
+            }
+            else {
+                pastPtr->next = newItem;
+                return start;
+            }
         }
         pastPtr = nextPtr;
         nextPtr = nextPtr->next;
@@ -74,10 +120,35 @@ LinkedHandBidList * insertSorted(LinkedHandBidList * start, LinkedHandBidList * 
 int main(int argc, char** args) {
     if (argc == 2) {
         FILE* handBidFile = fopen(args[1], "r");
-        Node * start;
+        LinkedHandBidList * start = NULL;
         char iobuffer[32];
+        if (handBidFile) {
+            while (fgets(iobuffer, 32, handBidFile) != NULL) {
+                printf("%s", iobuffer);
+                struct Node * newItem = malloc(sizeof(struct Node));
+                for (int i = 0; i < 5; i++) {
+                    newItem->handBid.ordering[i] = parseCard(iobuffer[i]);
+                }
+                newItem->handBid.bid = strtol(&(iobuffer[5]), NULL, 10);
+                start = insertSorted(start, newItem);
+            }
+            fclose(handBidFile);
+            LinkedHandBidList * temp = start;
+            int winnings = 0;
+            int rank = 1;
+            while (temp) {
+                printf("%d\n", temp->handBid.bid);
+                winnings += rank * temp->handBid.bid;
+                rank++;
+                temp = temp->next;
+            }
+            printf("winnings: %d", winnings);
+        }
+        else {
+            printf("Error: unable to open file %s \n", args[1]);
+        }
     }    
     else {
-        fputs("Argument Error, requires 1 argument of HandBid file", stdout);
+        printf("Argument Error, requires 1 argument of HandBid file \n");
     }
 }
